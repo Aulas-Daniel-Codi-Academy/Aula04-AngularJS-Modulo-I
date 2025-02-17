2600783048d1dd0df4b842a728652496deea5cda
Daniel Thielmann <daniel.thielmann@example.com>
2025-02-17 10:28:48 -0300

    build done, starting to code

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [],
+})
+export class AppComponent {
+  title = 'simulador-sorteio';
+}
