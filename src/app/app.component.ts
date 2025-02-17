6d89f2294e0e43ec2db2ec92cff9c89355f695d3
Daniel Thielmann <daniel.thielmann@example.com>
2025-02-17 11:28:07 -0300

    build clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [],
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'simulador-sorteio';
-}
+export class AppComponent {}
